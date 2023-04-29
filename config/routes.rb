be724eefa60bd3d4d7a2e8a410eb163dbc0c3c15
Kitazawa <121933368+kitacat51@users.noreply.github.com>
2023-04-29 09:06:42 +0000

    コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,37 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  
+# 顧客用 
+  devise_for :users, skip: [:passwords],controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+# 管理者用
+ devise_for :admin,skip: [:registrations, :passwords] , controllers: {
+     sessions: "admin/sessions"
+   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  scope module: :public do
+    root to: "homes#top" 
+  
+    get "/about" =>"homes#about",as: 'about'
+    get "/users/mypage" =>"users#mypage",as: 'mypage'
+    get "/users/information/edit" =>"users#edit",as: 'edit'
+    patch "/users/information" =>"users#update",as: 'update'
+    resources :movies
+  
+  
+  end
+  
+   get '/admin' => "admin/homes#top"
+  namespace :admin do
+ 
+    resources :movies
+    resources :users
+    resources :homes
+    resources :genres
+ 
+  end
+ 
 end
