5a1079ae1b0de8e8728c432728a216f7857df166
Kitazawa <121933368+kitacat51@users.noreply.github.com>
2023-05-07 12:34:10 +0000

    API取得

--- app/controllers/public/movies_controller.rb
+++ app/controllers/public/movies_controller.rb
@@ -1,2 +1,13 @@
 class Public::MoviesController < ApplicationController
+
+ require 'themoviedb-api'
+  Tmdb::Api.key("3804a932a84eed879946981a8af29cbf")
+  Tmdb::Api.language("ja")
+    
+ def search
+ end
+
+ def show
+ end
+    
 end
